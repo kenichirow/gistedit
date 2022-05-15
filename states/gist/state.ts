aa2bc9192e0969efa1e232b14e26d86c739a5a57
kenichirow <allagesxxx@gmail.com>
2022-05-15 20:27:24 +0900

    wip

--- states/gist/state.ts
+++ states/gist/state.ts
@@ -1,4 +1,10 @@
-import { atom, selector, selectorFamily } from "recoil";
+import {
+  atom,
+  atomFamily,
+  selector,
+  selectorFamily,
+  useRecoilCallback,
+} from "recoil";
 
 import { Gist, GistFile, RawGistFile, RawGistFileURL } from "./type";
 import { accessTokenQuery } from "../access_token";
@@ -34,8 +40,73 @@ export const userGistsQuery = selector<Array<Gist>>({
   },
 });
 
+const fetchGists = useRecoilCallback(({ snapshot, set }) => async () => {
+  const githubToken = await snapshot.getPromise(accessTokenQuery);
+  const user = await snapshot.getPromise(githubUserQuery);
+  const url = `https://api.github.com/users/${user.login}/gists`;
+  const data = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `token ${githubToken}`,
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        return Promise.reject();
+      }
+      return res.json();
+    })
+    .catch((error) => {
+      return error;
+    });
+
+  set(gistsAtom, () => {
+    return data;
+  });
+
+  data.forEach((gist: Gist) => {
+    set(gistAtom(gist.url), () => {
+      return gist;
+    });
+  });
+});
+
 export const currentGistIdState = atom<string>({
   key: "myapp.kenichirow.com:gist:current:id",
+});
+
+export const gistAtom = atomFamily<Gist, string>({
+  key: "myapp.kenichirow.com:gist1",
+});
+
+export const gistsAtom = atom<Gist[]>({
+  key: "myapp.kenichirow.com:gists2",
+});
+
+const setGists = useRecoilCallback(({ set }) => (gists: Gist[]) => {
+  // fetch
+  set(gistsAtom, () => {
+    return gists;
+  });
+  gists.forEach((gist) => {
+    set(gistAtom(gist.url), () => {
+      return gist;
+    });
+  });
+});
+
+const getGist = useRecoilCallback(({ snapshot }) => async (id: string) => {
+  return snapshot.getPromise(gistAtom(id));
+});
+
+const getCurrentGist = useRecoilCallback(({ snapshot }) => () => {
+  const id = snapshot.getLoadable(currentGistIdState).getValue();
+  return snapshot.getLoadable(gistAtom(id));
+});
+
+const getGists = useRecoilCallback(({ snapshot }) => () => {
+  return snapshot.getLoadable(gistsAtom);
 });
 
 export const currentGistQuery2 = selector({
